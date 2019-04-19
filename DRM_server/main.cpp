6c341e0180eebd0e32ee995c15fee819b237543f
spwpun <spwpun@gmail.com>
2019-04-19 13:54:18 +0800

    实现文件加解密功能测试！

--- DRM_server/main.cpp
+++ DRM_server/main.cpp
@@ -1,7 +1,13 @@
 #include <string.h>
 #include <stdio.h>
 #include "sm4.h"
+#define UNICODE
+#define _UNICODE
+
 #include <Windows.h>
+#include <tchar.h>
+BOOL encryptfile (const char *in_fname, unsigned char *key, const char *out_fname);
+BOOL decryptfile (const char *in_fname, unsigned char *key, const char *out_fname);
 
 int main ()
 {
@@ -10,44 +16,130 @@ int main ()
 	unsigned char output[16];
 	sm4_context ctx;
 	unsigned long i;
+	bool flag = FALSE;
 
 	printf ("加密密钥：");
 	for (i = 0; i < 16; i++)
 		printf ("%02X ", key[i]);
 	printf ("\n");
-
+		
 	//加密标准的测试数据
 	sm4_setkey_enc (&ctx, key);//设置key
 	printf ("加密前数据：");
 	for (i = 0; i < 16; i++)
 		printf ("%02X ", input[i]);
 	printf ("\n");
-	sm4_crypt_ecb (&ctx, 1, 16, input, output);
+	sm4_crypt_ecb (&ctx, SM4_ENCRYPT, 16, input, output);
 	printf ("加密后数据：");
 	for (i = 0; i < 16; i++)
 		printf ("%02X ", output[i]);
 	printf ("\n");
 
-	//decrypt testing
+	//解密测试
 	sm4_setkey_dec (&ctx, key);
-	sm4_crypt_ecb (&ctx, 0, 16, output, output);
+	sm4_crypt_ecb (&ctx, SM4_DECRYPT, 16, output, output);
 	printf ("解密后数据：");
 	for (i = 0; i < 16; i++)
 		printf ("%02X ", output[i]);
 	printf ("\n");
-
-	//decrypt 1M times testing vector based on standards.
-	/*i = 0;
-	sm4_setkey_enc (&ctx, key);
-	while (i < 1000000)
-	{
-		sm4_crypt_ecb (&ctx, 1, 16, input, input);
-		i++;
-	}
-	for (i = 0; i < 16; i++)
-		printf ("%02x ", input[i]);
-	printf ("\n");*/
-
+	printf ("文件加密测试：\n");
+	flag = encryptfile ("test.pdf", key, "encry_pdf.erc");
+	if (flag)	printf ("文件加密成功！\n");
+	flag = decryptfile ("encry_pdf.erc", key, "decry_pdf.pdf");
+	if (flag)	printf ("文件解密成功！\n");
 	system ("pause");
 	return 0;
+}
+
+BOOL encryptfile (const char *in_fname, unsigned char *key, const char *out_fname)
+{
+	FILE *fp1, *fp2;
+	int i = 0;
+	unsigned char ch;
+	//16个字节为一组，先试试，最后还是得分组加密
+	unsigned char input[16];
+	unsigned char output[16];
+	sm4_context ctx;//sm4上下文
+
+	sm4_setkey_enc (&ctx, key);//设置key
+	fp1 = fopen (in_fname, "rb");
+	if (fp1 == NULL)
+	{
+		printf ("文件打开失败，错误码:%d\n.", GetLastError ());
+		return FALSE;
+	}
+	fp2 = fopen (out_fname, "wb");
+	if (fp2 == NULL)
+	{
+		printf ("文件打开失败，错误码:%d\n.", GetLastError ());
+		return FALSE;
+	}
+	
+	while (1)
+	{
+		/*每次读入16个字节，使用SM4对称加密算法进行加密*/
+		for (i = 0; i < 16; i++)
+		{
+			if (feof (fp1)) break;	//如果读到文件尾则跳出循环
+			input[i] = fgetc (fp1);
+		}
+		sm4_crypt_ecb (&ctx, SM4_ENCRYPT, 16, input, output);	//加密，输出到output数组,参数2为定义的宏，标志是加密还是解密
+		for (i = 0; i < 16; i++)			
+		{
+			putc (output[i], fp2);		//将加密结果写入到文件中
+			output[i] = '\0';			//清除缓冲区
+			input[i] = '\0';
+		}
+		if (feof (fp1)) break;
+	}
+		
+	fclose (fp1);
+	fclose (fp2);
+	return TRUE;
+}
+
+BOOL decryptfile (const char *in_fname, unsigned char *key, const char *out_fname)
+{
+	FILE *fp1, *fp2;
+	int i = 0;
+	char ch;
+	unsigned char input[16];
+	unsigned char output[16];
+	sm4_context ctx;//sm4上下文
+
+	sm4_setkey_dec (&ctx, key);//设置解密key
+	fp1 = fopen (in_fname, "rb");
+	if (fp1 == NULL)
+	{
+		printf ("文件打开失败，错误码:%d\n.", GetLastError ());
+		return FALSE;
+	}
+	fp2 = fopen (out_fname, "wb");
+	if (fp2 == NULL)
+	{
+		printf ("文件打开失败，错误码:%d\n.", GetLastError ());
+		return FALSE;
+	}
+	while (1)
+	{
+		/*每次读入16个字节，使用SM4对称加密算法进行加密*/
+		for (i = 0; i < 16; i++)
+		{
+			if (feof (fp1)) break;	//如果读到文件尾则跳出循环
+			input[i] = fgetc (fp1);
+		}
+		if (feof (fp1)) break;
+		sm4_crypt_ecb (&ctx, SM4_DECRYPT, 1, input, output);
+		for (i = 0; i < 16; i++)			//将解密结果写入到文件中
+		{
+			putc (output[i], fp2);
+			//清除缓冲区
+			output[i] = '\0';
+			input[i] = '\0';
+		}
+	}
+
+	fclose (fp1);
+	fclose (fp2);
+	return TRUE;
 }
